7aa89d32acf3f7b7c34f3fd0171a9115f788381e
Volkan Özçelik <volkan.ozcelik@gmail.com>
2017-07-09 14:56:47 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,24 @@
 'use strict';
+
+/*         .°
+ *        |
+ *     .-----.
+ *     | | | |       Byte-Sized JavaScript
+ *     \  °  /          Module Generator
+ *   ___-___-___      https://bytesized.tv/
+ *  ()_   .   _()
+ *  /  /\ : /\  \
+ * (___)| . |(___)
+ *
+ * This project is a part of the “Byte-Sized JavaScript” videocast.
+ *
+ * You can watch “Byte-Sized JavaScript” at: https://bytesized.tv/
+ *
+ * MIT Licensed — See LICENSE.md
+ *
+ * Send your comments, suggestions, and feedback to me@volkan.io
+ */
+
 var path = require('path');
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
