5f5a1c753d3906b3957db32069b26ee46b98c388
Volkan Özçelik <volkan.ozcelik@gmail.com>
2017-07-09 14:56:10 -0700

    Update index.js

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,3 +1,22 @@
 'use strict';
 
+/*         .°
+ *        |
+ *     .-----.
+ *     | | | |       Byte-Sized JavaScript
+ *     \  °  /          Module Generator
+ *   ___-___-___      https://bytesized.tv/
+ *  ()_   .   _()
+ *  /  /\ : /\  \
+ * (___)| . |(___)
+ *
+ * This project is a part of the “Byte-Sized JavaScript” videocast.
+ *
+ * You can watch “Byte-Sized JavaScript” at: https://bytesized.tv/
+ *
+ * MIT Licensed — See LICENSE.md
+ *
+ * Send your comments, suggestions, and feedback to me@volkan.io
+ */
+
 module.exports = require( './release' );
